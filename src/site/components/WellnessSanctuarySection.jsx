8583e60c3b3cbf433c8f421626e141fad09b249a
Jerome Recipon <jerome.recipon@gmail.com>
2026-03-27 23:26:35 -0400

    content: Replace jacuzzi sunset photo with new mosaic hammam photo

--- src/site/components/WellnessSanctuarySection.jsx
+++ src/site/components/WellnessSanctuarySection.jsx
@@ -5,7 +5,7 @@ const WellnessSanctuarySection = () => {
     const images = [
         "/ayana/photos/v2/spa-nouveau-2.jpg",
         "/ayana/photos/v2/japandi-towels.png",
-        "/ayana/photos/v2/spa-nouveau-3.jpg",
+        "/ayana/photos/v2/spa-hammam-luxury.jpg",
         "/ayana/photos/v2/spa-sauna-benches.jpg",
         "/ayana/photos/v2/spa-nouveau-1.jpg",
         "/ayana/photos/v2/spa-nouveau-4.jpg"
